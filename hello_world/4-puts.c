d7583e356c7f9f8e7e53d2b5013d7952ee80cde7
jgnacio <jgnaciogomez@gmail.com>
2022-09-27 07:23:34 -0700

    fix ;

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,8 @@
 
 int main () {
 	char const_str[52];
-	strcpy(const_str, "\"Programming is like building a multilingual puzzle")
-	puts(const_str)
+	strcpy(const_str, "\"Programming is like building a multilingual puzzle");
+	puts(const_str);
 	return (0);
 
 }
