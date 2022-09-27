810ad42d6eb4e222030e4da78a20e5286af74df4
jgnacio <jgnaciogomez@gmail.com>
2022-09-27 07:22:40 -0700

    add 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+	char const_str[52];
+	strcpy(const_str, "\"Programming is like building a multilingual puzzle")
+	puts(const_str)
+	return (0);
+
+}
