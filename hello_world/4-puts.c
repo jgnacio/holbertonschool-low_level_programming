8e0208c304e114b220c54a76876b5afc88ff7665
jgnacio <jgnaciogomez@gmail.com>
2022-09-27 09:34:17 -0700

    fix betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	char const_str[52];
+
 	strcpy(const_str, "\"Programming is like building a multilingual puzzle");
 	puts(const_str);
 	return (0);
-
 }
