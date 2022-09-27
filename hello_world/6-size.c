257d0ab2fc8bca0864d27fed2183369648ee4312
jgnacio <jgnaciogomez@gmail.com>
2022-09-27 10:53:35 -0700

    fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	int myInt;
 	char myChar;
 	long int myLongInt;
-	long myLongLongInt;
+	long long myLongLongInt;
 	float myFloat;
 
 	printf("Size of a char: %ld byte(s) \n", sizeof(myChar));
