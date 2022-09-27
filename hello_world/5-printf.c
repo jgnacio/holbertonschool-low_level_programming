0a5d6ac85cda5ba794d60bd190f1db6bec5ed61a
jgnacio <jgnaciogomez@gmail.com>
2022-09-27 09:50:27 -0700

    add 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ ** main - Entry point
+ **
+ ** Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	char Art[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", Art);
+	return (0);
+}
