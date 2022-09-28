60d8d3d9d713c6105a0de54867c2c88bbdc40387
jgnacio <jgnaciogomez@gmail.com>
2022-09-28 11:08:31 -0700

    add 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet whit putchar
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 9; num++)
+	{
+		int putchar(int num);
+	}
+	putchar('\n');
+	return (0);
+}
