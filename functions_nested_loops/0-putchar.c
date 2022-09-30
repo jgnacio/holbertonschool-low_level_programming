1a26cad4cea725b793921a39e06c5e2c8abd9439
jgnacio <jgnaciogomez@gmail.com>
2022-09-30 06:39:17 -0700

    add 9

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the start function
+ *
+ * description: Write a program that prints _putchar,
+ * followed by a new line.
+ *
+ * Return: The program should return 0
+ */
+int main(void)
+{
+	char text[]= "_putchar";
+	int i = 0;
+	for(; i <= 7; i++)
+	{
+		putchar(text[i]);
+	}
+	putchar('\n');
+	return (0);
+}
