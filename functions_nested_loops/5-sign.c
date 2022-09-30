ee3b70202f5028b14f54c7e6b886a2395b0df6ac
jgnacio <jgnaciogomez@gmail.com>
2022-09-30 13:04:26 -0700

    fix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,9 +5,9 @@
  *
  * description: Write a function that checks for alphabetic character.
  *
- * Returns 1 and prints + if n is greater than zero
- * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
