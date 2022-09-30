f486d50798d87a73b8186c9b532d99c6ba96571f
jgnacio <jgnaciogomez@gmail.com>
2022-09-30 12:34:40 -0700

    add 3-islower and update main.h

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lowercase character
+ * @c: integer for check
+ *
+ * description: function that checks for lowercase character
+ *
+ * Return: The program should return 0
+ */
+int _islower(int c)
+{
+	if (c >= 27 && c <= 122)
+	{
+		_putchar('1');
+	}
+	return (0);
+}
