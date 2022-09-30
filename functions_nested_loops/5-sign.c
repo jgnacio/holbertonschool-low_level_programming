1891b11cb38c5b40be9ada35eaa84ff97795c130
jgnacio <jgnaciogomez@gmail.com>
2022-09-30 12:59:54 -0700

    add 5-sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Write a function that prints the sign of a number.
+ * @n: integer for check
+ *
+ * description: Write a function that checks for alphabetic character.
+ *
+ * Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
