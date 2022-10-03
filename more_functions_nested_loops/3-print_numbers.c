f7487b2010c77628b0fd85eb6caf775f49b5f67e
jgnacio <jgnaciogomez@gmail.com>
2022-10-03 11:13:18 -0700

    fix betty style

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,13 @@
 /**
  * print_numbers - Write a function that prints the numbers, from 0 to 9
  * , followed by a new line.
- * @a: firts number to multiplies
- * @b: second number to multiplies
  *
  * Return: result of a * b
  */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
