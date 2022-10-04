e3c7efb0d8659ee53952614777678cfd5ffd3db8
jgnacio <jgnaciogomez@gmail.com>
2022-10-04 10:14:25 -0700

    fix betty style

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - Write a function that draws a diagonal line on the terminal.
+ * print_diagonal - Write a function that draws a diagonal line on the
+ * terminal.
  * @n: number the diagonal to print
  *
  * Return: nothing :)
@@ -19,7 +20,7 @@ void print_diagonal(int n)
 				_putchar(' ');
 			}
 			_putchar(92);
-			if (i != n-1)
+			if (i != n - 1)
 			{
 				_putchar('\n');
 			}
