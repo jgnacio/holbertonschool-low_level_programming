eac4bf074feca4133365a8c8e8896bd8bac11c8d
jgnacio <jgnaciogomez@gmail.com>
2022-10-08 09:21:53 -0700

    fix space black in the end

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,9 @@ void puts_half(char *str)
 {
 	int i;
 	int len = _strlen(str);
-	int half = len / 2;
+	int half = (len - 1) / 2;
 
-	str += half;
+	str += half + 1;
 	for (i = half; i < len; i++)
 	{
 		_putchar(*str);
