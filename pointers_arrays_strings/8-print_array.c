340942f0caed85bc3f23264e7ce598680d6e7eaf
jgnacio <jgnaciogomez@gmail.com>
2022-10-11 05:06:00 -0700

    fix wrong length

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,18 +12,15 @@
 void print_array(int *a, int n)
 {
 	int i;
-	if (n < 0)
-	{
-		n = 0;
-	}
 
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
 		{
-			printf("%d\n", a[i]);
+			printf("%d", a[i]);
 			break;
 		}
 		printf("%d, ", a[i]);
 	}
+	printf("\n");
 }
