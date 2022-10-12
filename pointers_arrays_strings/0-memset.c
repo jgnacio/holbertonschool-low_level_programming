822971eaa67c6452f20e81d9fd4fa67daf840d32
jgnacio <jgnaciogomez@gmail.com>
2022-10-12 11:03:08 -0700

    fix betty documentation

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _memset - function that fills memory with a constant byte.
+ * @n: limiter to overwrite.
+ * @b: value to remplace
+ * @s: string to remplace.
+ *
+ * Return: string modified.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
