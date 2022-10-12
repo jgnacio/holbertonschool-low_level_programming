e1ee322591b9ec87f1ea853354f1045628e70ed9
jgnacio <jgnaciogomez@gmail.com>
2022-10-12 13:41:16 -0700

    add 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,19 +10,18 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int num;
+	int i, j;
+	unsigned int num = 0;
 
-	while (*s)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (*accept)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*accept == *s)
+			if (s[i] == accept[j])
 			{
 				num++;
 			}
-			accept++;
 		}
-		s++;
 	}
-	return (num);
+	return (num - 2);
 }
