ca692637bfbbc434f35765e895f5d10c575e1a4a
jgnacio <jgnaciogomez@gmail.com>
2022-10-12 06:38:43 -0700

    add 6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string -  function that capitalizes all words of a string.
+ * @str: string to set capitalize
+ *
+ * Return: String upper.
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	int separators[] = {32, 9, 10, 44, 46, 59, 49, 33, 63, 34, 40, 41, 123, 125};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (separators[j] == str[i])
+			{
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (str);
+}
