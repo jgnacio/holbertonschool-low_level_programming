8478c847ea81158de686b2e55ec206cf04e872a8
jgnacio <jgnaciogomez@gmail.com>
2022-10-14 06:39:54 -0700

    add 0-puts_recursion.c and main.h

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
