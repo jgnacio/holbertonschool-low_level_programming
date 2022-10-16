050ca9a1739c904be14f6c0207d1cb722ed249d2
jgnacio <jgnaciogomez@gmail.com>
2022-10-16 11:20:57 -0700

    fix betty documentation

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: the string to print reverse
  *
- *
- *
- *
+ * Return: Nothing.
  */
 void _print_rev_recursion(char *s)
 {
