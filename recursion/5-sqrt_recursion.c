a409aa3b4506cf0bb7c9640f37e2a10ed6853654
jgnacio <jgnaciogomez@gmail.com>
2022-10-16 11:43:03 -0700

    fix betty doc

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,23 +2,26 @@
 
 int check_sqrt(int a, int b);
 /**
+ * _sqrt_recursion - function that returns the natural square
+ * root of a number.
+ * @n: number to calculate the sqrt
  *
- *
- *
- *
+ * Return: the sqrt of n.
  */
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
 
-	return check_sqrt(1, n);
+	return (check_sqrt(1, n));
 }
 
 /**
+ * check_sqrt - function that check sqrt.
+ * @a: number to multiply and check if is b
+ * @b: number inside the sqrt
  *
- *
- *
+ * Return: -1 if not is the sqrti, and the sqrt.
  */
 int check_sqrt(int a, int b)
 {
@@ -26,5 +29,5 @@ int check_sqrt(int a, int b)
 		return (a);
 	if (a * a > b)
 		return (-1);
-	return check_sqrt(a + 1, b);
+	return (check_sqrt(a + 1, b));
 }
