d502396407cc46211a3caa44c13642aab36be2b3
jgnacio <jgnaciogomez@gmail.com>
2022-10-17 13:12:09 -0700

    fix

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _isdigit(char *d);
 /**
  * main - program that prints its name, followed by a new line.
  * @argc: argc.
@@ -19,11 +20,36 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	for (i = 0; i < argc; i++)
+	for (i = 1, result = 0; i < argc; i++)
 	{
-		result += atoi(argv[i]);
+		if (_isdigit(argv[i]))
+			result += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
 	printf("%d\n", result);
-
 	return (0);
+}
+
+/**
+ * _isdigit - function to check if it´s a number
+ * @d: string to verify
+ *
+ * Return: 1 if is a number or 0 if not.
+ */
+int _isdigit(char *d)
+{
+	int i;
+
+	for (i = 0; d[i]; i++)
+	{
+		if (d[i] < 48 || d[i] > 57)
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
