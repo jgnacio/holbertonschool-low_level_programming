18d9e72fde34ace7edde5834c8df87d257056118
jgnacio <jgnaciogomez@gmail.com>
2022-10-17 09:16:00 -0700

    fix

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - program that prints its name, followed by a new line.
+ * @argc: argc.
+ * @argv: argv.
  *
- *
- *
+ * Return: 0.
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (argc);
+	return (0);
 }
