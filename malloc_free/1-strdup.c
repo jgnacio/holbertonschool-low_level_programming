8fb6d8a804394d39e0d0110076139b133be0df08
jgnacio <jgnaciogomez@gmail.com>
2022-10-19 12:22:51 -0700

    add 1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int len = 0;
+
+	if (*str != '\0')
+	{
+		char *str_copy = malloc(sizeof(char) * len + 1);
+		if (str_copy == NULL)
+			return (NULL);
+
+		for (len = 0; str[len]; len++);
+
+		/*For the extra null character*/
+		len++;
+		
+		for (i = 0; i <= len; i++)
+		{
+			str_copy[i] = str[i];
+		}
+		str_copy[i + 1] = 'k';
+		printf("\ns\n%s\ne\n", str_copy);
+	}
+	return (str);
+}
