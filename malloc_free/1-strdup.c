70a862032fca0fe08ee0c324d07fc5c0f16e0a97
jgnacio <jgnaciogomez@gmail.com>
2022-10-19 12:27:20 -0700

    fix

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,18 +10,19 @@ char *_strdup(char *str)
 {
 	int i = 0;
 	int len = 0;
+	char *str_copy = NULL;
 
 	if (*str != '\0')
 	{
-		char *str_copy = malloc(sizeof(char) * len + 1);
-		if (str_copy == NULL)
-			return (NULL);
-
 		for (len = 0; str[len]; len++);
 
 		/*For the extra null character*/
-		len++;
-		
+
+		str_copy = malloc(sizeof(char) * len + 1);
+
+		if (str_copy == NULL)
+			return (NULL);
+
 		for (i = 0; i <= len; i++)
 		{
 			str_copy[i] = str[i];
