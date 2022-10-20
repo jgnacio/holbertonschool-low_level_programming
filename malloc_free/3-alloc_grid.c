0e98cc10338cc7888049efd47783696fdaca86af
jgnacio <jgnaciogomez@gmail.com>
2022-10-20 10:47:52 -0700

    fix the space

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,10 +2,12 @@
 
 
 /**
+ * alloc_grid - function that returns a pointer to a 2
+ * dimensional array of integers.
+ * @width: is the width of the array.
+ * @height: is the height of the array.
  *
- *
- *
- *
+ * Return: full grid.
  */
 int **alloc_grid(int width, int height)
 {
@@ -16,24 +18,19 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int) * (height * width));
+	grid = malloc(sizeof(*grid) * height);
 
 	if (!grid)
 		return (NULL);
-	else
-	{
-		for (i = 0; i <= height; i++)
-		{
-			grid[i] = malloc(sizeof(int) * width);
-		}
+	for (i = 0; i < height; i++)
+		grid[i] = malloc(sizeof(int) * width);
 
-		for (x = 0; x < height; x++)
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
 		{
-			for (y = 0; y < width; y++)
-			{
-				grid[x][y] = 0;
-			}
+			grid[x][y] = 0;
 		}
 	}
-	return(grid);
+	return (grid);
 }
