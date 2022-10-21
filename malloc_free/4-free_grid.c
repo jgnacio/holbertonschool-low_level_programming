4d115f71366873cdf0e7cc3be9d180e3cd0b4a54
jgnacio <jgnaciogomez@gmail.com>
2022-10-21 03:22:48 -0700

    add 4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * free_grid - function to free a dynamic 2 dimencional array.
+ * @grid: is a dynamic 2 dimencional array
+ * @height: the first pointers pointing to the second dimencion
+ *
+ * Return: Nothing :).
+ */
+void free_grid(int **grid, int height)
+{
+	for (; height >= 0; height--)
+		free(grid[height]);
+	free(grid);
+}
