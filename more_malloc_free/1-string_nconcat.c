24780ca7d5c7aaa56934db4568988456b7ee3c19
jgnacio <jgnaciogomez@gmail.com>
2022-10-22 06:33:33 -0700

    fix null string len and betty style and doc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,16 +3,27 @@
 #include <string.h>
 #include "main.h"
 
-char *string_nconcat(char *s1, char *s2,__attribute__((unused)) unsigned int n)
+/**
+ * string_nconcat - function that concatenates two strings.
+ * @s1: String 1 to concat.
+ * @s2: String 2 to concat.
+ * @n: How long of s2 to copy.
+ *
+ * Return: String concatenates.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *stct = NULL;
-	unsigned int i = 0, lens1 = strlen(s1), lens2 = strlen(s2);
+	unsigned int i = 0, lens1, lens2;
 	int j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	lens1 = strlen(s1);
+	lens2 = strlen(s2);
 
 	if (n > lens2)
 		n = lens2;
