a915eba40b35e330d0b88e2f28bb583636e0ac2e
jgnacio <jgnaciogomez@gmail.com>
2022-10-22 07:39:31 -0700

    fix the range of set 0

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (newarray == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		newarray[i] = 0;
 
 	return (newarray);
