c712edfdec7507067f0448f77dc11c27579d22ff
jgnacio <jgnaciogomez@gmail.com>
2022-10-24 05:42:47 -0700

    add 2-main.c

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+/**
+ * main - program that prints the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ *	Return: Zero.
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+	return(0);
+}
