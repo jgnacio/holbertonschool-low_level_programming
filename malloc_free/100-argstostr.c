03881d749448b4b853701ed702c162f0ad2a5eca
jgnacio <jgnaciogomez@gmail.com>
2022-10-26 06:46:20 -0700

    add 100-argstostr.c

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - function that concatenates all the arguments of your program.
+ * @ac: Argument count.
+ * @av: Argument vector.
+ *
+ * Return: Pointer to a string.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *argstr = NULL;
+	int i = 0, j = 0, k = 0;
+	int count = 0;
+
+	for (i = 0; i < ac; i++, count++)
+		for (j = 0; av[i][j]; j++, count++)
+		;
+	;
+
+	/* fix the blank spaces*/
+	count -= 2;
+
+	/* Reserve on memory the size require for alloc the words*/
+	argstr = malloc(count);
+
+	if (argstr == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++, k++)
+	{
+		for (j = 0; av[i][j]; j++, k++)
+			argstr[k] = av[i][j];
+		argstr[k] = '\n';
+	}
+
+	return (argstr);
+}
