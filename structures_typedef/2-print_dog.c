4c3ea73af31c6d4b7cdc8aae3505368bdc10dc45
jgnacio <jgnaciogomez@gmail.com>
2022-10-27 08:40:58 -0700

    fix nil on floats

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,11 +14,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		if (d->name != NULL)
-			printf("Age: %.6f\n", d->age);
-		else
-			printf("Age: (nil)\n");
-		if (d->name != NULL)
+		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)\n");
