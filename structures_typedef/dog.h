85aa0a408708d22b53d96483a8cdb48687598cda
jgnacio <jgnaciogomez@gmail.com>
2022-10-27 07:20:09 -0700

    add dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - New type for my dogs
+ * @name: Name of dogs.
+ * @age: The age.
+ * @owner: owner.
+ */
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
+#endif
