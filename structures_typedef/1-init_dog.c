6908f77e56ea44762d967c6ac7bc4bfae4c36f72
jgnacio <jgnaciogomez@gmail.com>
2022-10-27 08:15:53 -0700

    fix NULL

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - function that initialize a variable of type struct dog.
  * @d: a pointer pointing to my_dog on main.c
@@ -8,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
