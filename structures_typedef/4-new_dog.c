548042331ccf37f1e400298b952db59468ca1f7f
jgnacio <jgnaciogomez@gmail.com>
2022-10-27 10:49:45 -0700

    fix string duplicates

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
+int _strlen(char *str);
 /**
  * new_dog - function that creates a new dog.
  * @name: name of new dog.
@@ -11,13 +13,46 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i;
+	int lenname;
+	int lenowner;
 	dog_t dog_new;
 	dog_t *ptr;
 
-	dog_new.name = name;
-	dog_new.owner = owner;
-	dog_new.age = age;
-	ptr = &dog_new;
+	ptr = malloc(sizeof(dog_new));
+
+	if (name == NULL)
+		name = "";
+	if (owner == NULL)
+		name = "";
+
+	lenname = _strlen(name);
+	lenowner = _strlen(owner);
+
+	ptr->name = malloc(lenname);
+	ptr->owner = malloc(lenowner);
+	ptr->age = age;
+
+	for (i = 0; name[i]; i++)
+		ptr->name[i] = name[i];
+	
+	for (i = 0; owner[i]; i++)
+		ptr->owner[i] = owner[i];
 
 	return (ptr);
+}
+
+/**
+ * _strlen - function to count the characters of an string.
+ * @str: string to check.
+ *
+ * Return: count.
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len]; len++)
+		;
+	return (len);
 }
