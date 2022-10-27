dd881224848588d1750f66c41093d1d2a5aaed7d
jgnacio <jgnaciogomez@gmail.com>
2022-10-27 11:45:07 -0700

    hola

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -23,7 +23,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	ptr->name = strdup(name);
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
 	ptr->owner = strdup(owner);
+	if (ptr->owner == NULL)
+	{
+		free(ptr);
+		free(ptr->name);
+		return (NULL);
+	}
 	ptr->age = age;
 
 	return (ptr);
@@ -44,40 +56,4 @@ int _strlen(char *str)
 	for (len = 0; str[len]; len++)
 		;
 	return (len);
-}
-
-/**
- * _strdup - function that returns a pointer to a newly allocated space in
- * memory, which contains a copy of the string given as a parameter.
- * @str: string to copy.
- *
- * Return: the pointer of array.
- */
-char *_strdup(char *str)
-{
-	int i = 0;
-	int len = 0;
-	char *str_copy = NULL;
-
-	if (str != NULL)
-	{
-		for (len = 0; str[len]; len++)
-		{
-		}
-
-		/*For the extra null character*/
-
-		str_copy = malloc(sizeof(char) * len + 1);
-
-		if (str_copy == NULL)
-			return (NULL);
-
-		for (i = 0; i < len; i++)
-		{
-			str_copy[i] = str[i];
-		}
-		return (str_copy);
-	}
-	else
-		return (str_copy);
 }
