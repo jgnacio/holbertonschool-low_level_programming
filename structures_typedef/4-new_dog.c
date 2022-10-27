0c6b3a72b7f0622de970b3bca382e432f3e55d08
jgnacio <jgnaciogomez@gmail.com>
2022-10-27 11:40:13 -0700

    fix malloc on strdup

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 int _strlen(char *str);
@@ -21,8 +22,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ptr == NULL)
 		return (NULL);
 
-	ptr->name = _strdup(name);
-	ptr->owner = _strdup(owner);
+	ptr->name = strdup(name);
+	ptr->owner = strdup(owner);
 	ptr->age = age;
 
 	return (ptr);
