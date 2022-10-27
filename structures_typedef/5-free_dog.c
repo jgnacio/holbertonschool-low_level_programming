c61bce52df8e0b8433ddeabb892b11ee596c98bc
jgnacio <jgnaciogomez@gmail.com>
2022-10-27 15:29:02 -0700

    add 5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: struct dog_t.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
