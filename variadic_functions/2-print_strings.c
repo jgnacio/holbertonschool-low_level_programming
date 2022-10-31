b5ed390a7709f804c182f0733bc7a0ea00054595
jgnacio <jgnaciogomez@gmail.com>
2022-10-31 11:41:47 -0700

    fix NULL arguments

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -2,20 +2,26 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * print_strings - function that prints strings, followed by a new line.
+ * @separator: string to separate the words.
+ * @n: the argument before than
  *
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	char *aux;
 
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char*));
+		aux = va_arg(ap, char*);
+		if (aux == NULL)
+			printf("(nil)");
+		else
+			printf("%s", aux);
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
