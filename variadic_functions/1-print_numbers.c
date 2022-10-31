403aadd966b2ccf255c8137d7a4ea2c2c30dcf62
jgnacio <jgnaciogomez@gmail.com>
2022-10-31 09:14:59 -0700

    fix the separator NULL

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,14 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	if (separator)
-	{
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(ap, int));
-			if (i < n - 1)
+			if (i < n - 1 && separator)
 				printf("%s", separator);
 		}
-	}
 	printf("\n");
 }
