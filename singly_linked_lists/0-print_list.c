d06ed5802ce3c3ac1aa2fb1078aa367aac1eb7bf
jgnacio <jgnaciogomez@gmail.com>
2022-11-14 10:19:35 -0800

    fix betty style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include "lists.h"
 
- size_t print_list(const list_t *h)
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: the pointer to a new list_t from main.
+ *
+ * Return: the length of elements of list.
+ */
+size_t print_list(const list_t *h)
 {
 	int i = 0;
 
-	for(i = 0; h; i++)
+	for (i = 0; h; i++)
 	{
 		if (!h->str)
 		{
