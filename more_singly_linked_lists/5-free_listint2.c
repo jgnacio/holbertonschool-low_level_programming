183ca76d6d25562e67753d7df40e8bd64773f882
jgnacio <jgnaciogomez@gmail.com>
2022-11-17 04:58:48 -0800

    fix betty and NULL case

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,16 +1,23 @@
 #include "lists.h"
 
 /**
+ * free_listint2 - Function that set head to NULL
+ * @head: The pointer to the list.
+ */
+void free_listint2(listint_t **head)
+{
+	if (head)
+	{
+		free_listint(*head);
+		*head = NULL;
+	}
+}
+
+/**
  * free_listint - Function that frees a listint_t list.
  * @head: is the listint_t to free.
  *
  */
-void free_listint2(listint_t **head)
-{
-	free_listint(*head);
-	*head = NULL;
-}
-
 void free_listint(listint_t *head)
 {
 	if (head)
