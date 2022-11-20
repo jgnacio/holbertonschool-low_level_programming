282d50995b17845546982f86fa4867906ddfd882
jgnacio <jgnaciogomez@gmail.com>
2022-11-19 16:35:26 -0800

    fix return

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -34,9 +34,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 			*head = newnode;
 
+		return (newnode);
+	}
+	else
+	{
+		newnode = makenode(n);
+		*head = newnode;
 		return (*head);
 	}
-	return (NULL);
 }
 
 /**
