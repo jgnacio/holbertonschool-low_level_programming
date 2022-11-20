59a530cd2dc7f0ccbcc202608fe60bc6681ba350
jgnacio <jgnaciogomez@gmail.com>
2022-11-19 16:46:59 -0800

    fix

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		if (!aux)
 		{
-			if (prev->next == NULL)
+			if (prev->next == NULL && idx == 0)
 			{
 				prev->next = newnode;
 				return (newnode);
