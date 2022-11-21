07350433ae8bc3cf2e57737be58131ba8e3a7e09
jgnacio <jgnaciogomez@gmail.com>
2022-11-21 06:58:17 -0800

    fix 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,18 +14,15 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	struct stat st;
 	int fd;
 	int len_cont = strlen(text_content);
 
 	if (!filename)
 		return (-1);
 
-	fd = stat(filename, &st);
-	if (fd == 0)
-		return (1);
-
-	fd = open(filename, O_RDWR | O_CREAT);
+	fd = open(filename, O_RDWR | O_CREAT | O_EXCL);
+	if (fd == -1)
+		return (-1);
 
 	write(fd, text_content, len_cont);
 	return (1);
