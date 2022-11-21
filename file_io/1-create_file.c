c4eb6ee00cc6a481e26983cf0279023c23b6b5ec
jgnacio <jgnaciogomez@gmail.com>
2022-11-21 07:26:05 -0800

    fix NULL case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,10 +15,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int len_cont = strlen(text_content);
+	int len_cont = 0;
 
-	if (!filename || text_content == NULL)
+	if (!filename)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	len_cont = strlen(text_content);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
