d4dd9f8bf763f683f5d4127201206b4e94360658
jgnacio <jgnaciogomez@gmail.com>
2022-11-23 05:19:07 -0800

    fix end

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -41,7 +41,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	else if ((close(fd_from)) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-	if (free(buf) == NULL)
-		exit(101);
+	free(buf)
 	return (0);
 }
