130313c8811089bd287b26a48642e001c7027d59
jgnacio <jgnaciogomez@gmail.com>
2022-11-23 06:58:58 -0800

    add 0-print_dlistint.c

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - Function that prints all the elements of a listint_t list.
+ * @h: The linked list for print.
+ *
+ * Return: The num of elements.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int num_elem = 0;
+
+	if (h)
+	{
+		printf("%d\n", h->n);
+		num_elem++;
+		num_elem += print_dlistint(h->next);
+	}
+	return (num_elem);
+}
