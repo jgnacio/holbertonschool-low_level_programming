759df71f1eb4cfc0f7071f8d176e1a784b3db5a7
jgnacio <jgnaciogomez@gmail.com>
2022-11-23 13:28:19 -0800

    add 7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Function that inserts a new node
+ * at a given position.
+ * @h: It's the first node of the list.
+ * @idx: The index to return.
+ * @n: The number to add a new node.
+ *
+ * Return: The node based on index.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *aux = *h;
+	dlistint_t *newnode = NULL;
+	dlistint_t *prev = NULL;
+
+	newnode = makenode(n);
+	if (!newnode)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	if (h && *h)
+	{
+		for (; idx != 0 && aux; idx--)
+		{
+			prev = aux;
+			aux = aux->next;
+		}
+		if (!aux)
+		{
+			if (prev->next == NULL && idx == 0)
+			{
+				prev->next = newnode;
+				return (newnode);
+			}
+			return (NULL);
+		}
+		aux->prev = newnode;
+		newnode->next = aux;
+		if (prev)
+			prev->next = newnode;
+		else
+			*h = newnode;
+		return (newnode);
+	}
+	else
+	{
+		*h = newnode;
+		return (*h);
+	}
+}
