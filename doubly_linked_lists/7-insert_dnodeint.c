2ccc03a34c8f007a7cf24d4439016b43231830a6
jgnacio <jgnaciogomez@gmail.com>
2022-11-23 13:34:50 -0800

    fix return

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -43,7 +43,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			prev->next = newnode;
 		else
 			*h = newnode;
-		return (newnode);
+		return (*h);
 	}
 	else
 	{
