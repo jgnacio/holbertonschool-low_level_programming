639d6d1c8794996e4f9c7a9204ce69276111f6b4
jgnacio <jgnaciogomez@gmail.com>
2022-11-23 06:20:41 -0800

    update

--- file_io/main.h
+++ file_io/main.h
@@ -2,17 +2,11 @@
 #define FIO_H
 #include <stdio.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdarg.h>
 #include <stdlib.h>
-#include "1-create_file_2.c"
-#include "0-read_textfile_2.c"
 
 ssize_t read_textfile(const char *filename, size_t letters);
-char *read_textfile_2(const char *filename);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 #endif
