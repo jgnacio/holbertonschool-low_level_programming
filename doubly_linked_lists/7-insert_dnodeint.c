b1198c52d04e6c2fc1042208c381f8d1df80241a
jgnacio <jgnaciogomez@gmail.com>
2022-11-24 08:24:16 -0800

    fix betty

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!h)
 		return (NULL);
 	if (idx == 0)
-		return(add_dnodeint(h, n));
+		return (add_dnodeint(h, n));
 
 	aux = get_dnodeint_at_index(*h, idx - 1);
 	if (!aux)
