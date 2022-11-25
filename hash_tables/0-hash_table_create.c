00fedcea9854d0cd4d8ea5cb11266d7bd2d047da
jgnacio <jgnaciogomez@gmail.com>
2022-11-25 05:33:46 -0800

    fix the malloc size

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table = NULL;
 
-	new_hash_table = malloc(size);
+	new_hash_table = malloc(sizeof(hash_node_t) * size);
 	if (!new_hash_table)
 		return (NULL);
 	return (new_hash_table);
