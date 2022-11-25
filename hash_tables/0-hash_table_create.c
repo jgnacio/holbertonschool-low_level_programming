4f905143c74ce8b87fb8e869409d2eb0bd92485c
jgnacio <jgnaciogomez@gmail.com>
2022-11-25 06:26:30 -0800

    fix memory alocation and set null all pointers in array

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,8 +10,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table = NULL;
 
-	new_hash_table = malloc(sizeof(hash_node_t) * size);
+	new_hash_table = malloc(sizeof(hash_table_t));
 	if (!new_hash_table)
 		return (NULL);
+	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (!new_hash_table->array)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
+
+	new_hash_table->size = size;
+
+	while (--size)
+	{
+		new_hash_table->array[size] = NULL;
+	}
 	return (new_hash_table);
 }
