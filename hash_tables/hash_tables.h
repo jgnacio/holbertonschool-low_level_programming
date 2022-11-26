731dc0f15f38bb494d074df3fb8ba6194db4384d
jgnacio <jgnaciogomez@gmail.com>
2022-11-26 06:57:50 -0800

    update hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,6 +1,8 @@
 #ifndef HASH_H
 #define HASH_H
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -38,4 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+hash_node_t *add_nodesh(hash_node_t **head, const char *key, const char *value);
+hash_node_t *makenode(const char *key, const char *value);
+void print_table(hash_table_t *ht, int size);
 #endif
