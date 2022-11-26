36de89531a12e86f2321c576365eb30b573b2ed2
jgnacio <jgnaciogomez@gmail.com>
2022-11-26 07:07:03 -0800

    fix ht is NULL and malloc return

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,24 +12,33 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t **a_nodes = NULL;
+	hash_node_t *check_node = NULL;
 	unsigned long int c_index = 0;
 
 	/* Check if the node is not empty string */
 	if (strcmp(key, "") == 0)
 		return (0);
 
+	if (!ht)
+		return (0);
+
 	/* Get the array of nodes */
 	a_nodes = ht->array;
 
+	/* Get index with djb2 algorithm */
 	c_index = key_index((unsigned const char *)key, ht->size);
 	if (a_nodes[c_index])
 	{
+		/* If the key already exist update */
 		if (strcmp(a_nodes[c_index]->key, key) == 0)
 		{
 			(a_nodes[c_index])->value = strdup(value);
 			return (1);
 		}
 	}
+	/* Put a node at the beginning or add new one */
 	add_nodesh(&(a_nodes[c_index]), key, value);
+	if (!check_node)
+		return (0);
 	return (1);
 }
